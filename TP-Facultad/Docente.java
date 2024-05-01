0b2fe1ce18a36181702e75b105eedc5f66359928
Agus <aguscafiero22@gmail.com>
2024-05-01 18:43:27 -0300

    Actualizo docente

--- TP-Facultad/Docente.java
+++ TP-Facultad/Docente.java
@@ -27,4 +27,31 @@ public class Docente {
      */
     private int idDocente;
 
+    private Curso curso;
+
+
+    
+
+    public Docente(String nombre, String apellido, int idDocente) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.idDocente = idDocente;
+	}
+	
+	public Curso getCurso() {
+	        return curso;
+	    }
+
+	public void setCarrera(Curso curso) {
+	        this.curso = curso;
+	    }
+	
+
+	@Override
+	public String toString() {
+		return "Docente [nombre=" + nombre + ", apellido=" + apellido + ", idDocente=" + idDocente
+				+ ", cursosAsignados=" + /*listadoCursos +*/ "]";
+	}
+
 }
--- TP-Facultad/ProgramaPrincipal.java
+++ TP-Facultad/ProgramaPrincipal.java
@@ -6,7 +6,10 @@ public class ProgramaPrincipal {
 	public static void main(String[] args) {
 		
 		
-		Carrera carrera = new Carrera("Ingenieria en Sistemas");
+		
+		Carrera carrera1 = new Carrera("Ingenieria en Sistemas");
+		Carrera carrera2 = new Carrera("Derecho");
+		Carrera carrera3 = new Carrera("Ingenieria Quimica");
 		
 		ControladorCarrera controladorCarrera = new ControladorCarrera();
 		
@@ -18,10 +21,19 @@ public class ProgramaPrincipal {
 		
 	    controladorCarrera.agregarMateria(materias);
 		
-		Estudiante estudiante1 = new Estudiante("Maria", "Luzzi", 1234, true, carrera);
+		Estudiante estudiante1 = new Estudiante("Maria", "Luzzi", 1234, true, carrera1);
+		Estudiante estudiante2 = new Estudiante("Agustina", "Cafiero", 2234, true, carrera2);
+		Estudiante estudiante3 = new Estudiante("Yancamil", "Gomez Lopez", 7654, true, carrera3);
+		Estudiante estudiante4 = new Estudiante("Jose", "Sparks", 9987, true, carrera1);
 	
 		
-		estudiante1.setCarrera(carrera);
+		estudiante1.setCarrera(carrera1);
+		estudiante2.setCarrera(carrera2);
+		estudiante3.setCarrera(carrera3);
+		estudiante4.setCarrera(carrera1);
+		
+		Docente docente1 = new Docente ("Esteban", "Lamonte", 35);
+		Docente docente2 = new Docente ("Carmen", "Lopez", 22);
 		
 		// TODO Auto-generated method stub
 
