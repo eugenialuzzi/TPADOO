ea888beff4e525f3279040b081effb6c1a5dd748
tgomezl <116039364+yjgomez@users.noreply.github.com>
2024-05-02 18:12:55 -0300

    arreglos error agregar materia

--- TP-Facultad/controllers/ControladorCarrera.java
+++ TP-Facultad/controllers/ControladorCarrera.java
@@ -10,18 +10,20 @@ import model.Materia;
  */
 public class ControladorCarrera {
 
-	private List<Materia> materias;
+	private List<Materia> materias= new ArrayList<Materia>();
 	
     public ControladorCarrera() {
-    }
-
-	public void agregarMateria(List<Materia> materias) {
-	    this.materias.add((Materia) materias);
+    }         
+   
+	public void agregarMateria(List<Materia> materia) {
+		// Recorremos la lista de materiales que queremos agregar
+        for (Materia m : materia) {
+            this.materias.add(m);
 	}
-
+	}
     public Boolean validarCargaHorariaMax( Carrera carrera) {
         // TODO implement here
-        return null;
+        return false;
     }
 
 }
